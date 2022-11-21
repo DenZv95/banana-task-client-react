0e72754861e44c79dd6ec398a8d9cb85bdbf27d0
DenZv95 <den.zv95@yandex.ru>
2022-11-21 21:52:13 +0900

    заготовка главной страницы

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,12 +1,51 @@
 import React from 'react';
+import Button from '../../ui/Button/Button';
+import Field from '../../ui/Field/Field';
+import styles from './Home.module.scss';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
+  const todos = [
+    {
+      id: 1,
+      name: 'Task 1',
+      complete: false,
+    },
+    {
+      id: 2,
+      name: 'Task 2',
+      complete: false,
+    },
+    {
+      id: 3,
+      name: 'Task 3',
+      complete: false,
+    },
+    {
+      id: 4,
+      name: 'Task 4',
+      complete: false,
+    },
+  ];
   return (
-    <div>
-      Home
-      <div>
-        <Link to='/login'>Expenses</Link>
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <Link to='/login'>logout</Link>
+        </div>
+        <form className={styles.ToDoInput}>
+          <input type='text' placeholder='todo...' className={styles.input} />
+          <input type='submit' value='Add' />
+        </form>
+        <ul className={styles.ToDoList}>
+          {todos.map((todo) => {
+            return (
+              <li key={todo.id} className={styles.ToDoItem}>
+                {todo.name}
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </div>
   );

0c29e11d4759d85e55e736caf6de92991981efd2
DenZv95 <den.zv95@yandex.ru>
2022-11-23 19:03:06 +0900

    SignUp и SignIn перенос в Auth

--- src/components/pages/Auth/Auth.jsx
+++ src/components/pages/Auth/Auth.jsx
@@ -1,9 +1,69 @@
 import React, { useState } from 'react';
+import { useMutation } from 'react-query';
+import { useNavigate } from 'react-router-dom';
+import { $api } from '../../../api/Api';
+import { useAuth } from '../../../hooks/useAuth';
 import styles from './Auth.module.scss';
 
 const Auth = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const { setIsAuth } = useAuth();
+  let navigate = useNavigate();
+
+  const successLogin = (token) => {
+    localStorage.setItem('token', token);
+    setIsAuth(true);
+    setEmail('');
+    setPassword('');
+    navigate('/');
+  };
+
+  const {
+    mutate: auth,
+    isLoading: isLoadingAuth,
+    error: errorAuth,
+  } = useMutation(
+    'Auth',
+    () =>
+      $api({
+        url: '/users/login',
+        auth: false,
+        type: 'POST',
+        body: { email, password },
+      }),
+    {
+      onSuccess(data) {
+        console.log(data.token);
+        successLogin(data.token);
+      },
+    }
+  );
+
+  const {
+    mutate: register,
+    isLoading,
+    error,
+  } = useMutation(
+    'Registration',
+    () =>
+      $api({
+        url: '/users/register',
+        auth: false,
+        type: 'POST',
+        body: { email, password },
+      }),
+    {
+      onSuccess(data) {
+        successLogin(data.token);
+      },
+    }
+  );
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    auth();
+  };
 
   return (
     <div className={styles.limiter}>
@@ -12,7 +72,7 @@ const Auth = () => {
           <h1 className={styles.header}>Sign In</h1>
         </div>
 
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={handleSubmit}>
           <input
             placeholder='Email'
             type='email'

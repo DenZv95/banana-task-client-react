d465766f7e9580af6186ab688ee3a466888e5da8
DenZv95 <den.zv95@yandex.ru>
2022-11-24 19:30:37 +0900

    todoItem

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -5,6 +5,11 @@ import { useQuery } from 'react-query';
 import { $api } from '../../../api/Api';
 import { useAuth } from '../../../hooks/useAuth';
 
+import checkImage from '../../../images/check.svg';
+import trashImage from '../../../images/trash.svg';
+import settingsImage from '../../../images/settings.svg';
+import editImage from '../../../images/edit-2.svg';
+
 const Home = () => {
   const [todos, setTodos] = useState([]);
   let navigate = useNavigate();
@@ -37,20 +42,60 @@ const Home = () => {
             <button className={styles.button}>All</button>
             <button className={styles.button}>Active</button>
             <button className={styles.button}>Done</button>
-            <button className={styles.circleButton}>X</button>
+            <button className={styles.circleButton}>
+              <img
+                src={settingsImage}
+                width='29'
+                height='23'
+                alt='settings'
+                draggable={false}
+              />
+            </button>
           </div>
         </div>
-
         <ul className={styles.ToDoList}>
-          {todos.map((todo) => {
+          {todos.map((todo, id) => {
             return (
-              <li key={todo.id} className={styles.ToDoItem}>
-                {todo.name}
+              <li key={id} className={styles.ToDoItem}>
+                <div className={styles.todoDetails}>
+                  <button>
+                    <img
+                      src={checkImage}
+                      width='29'
+                      height='23'
+                      alt='check'
+                      draggable={false}
+                    />
+                  </button>
+                  <div className={styles.todoTexts}>
+                    <span> {todo.name}</span>
+                    <span>{todo.createdAt}</span>
+                  </div>
+                </div>
+                <div className={styles.todoActions}>
+                  <button>
+                    <img
+                      src={trashImage}
+                      width='29'
+                      height='23'
+                      alt='trash'
+                      draggable={false}
+                    />
+                  </button>
+                  <button>
+                    <img
+                      src={editImage}
+                      width='29'
+                      height='23'
+                      alt='edit'
+                      draggable={false}
+                    />
+                  </button>
+                </div>
               </li>
             );
           })}
         </ul>
-
         <div className={styles.addContainer}>
           <input type='text' placeholder='Todo' className={styles.input} />
           <button className={styles.button}>Add</button>

3305c1409ace6e899bddd530d445f625915be79c
DenZv95 <den.zv95@yandex.ru>
2022-12-01 13:55:47 +0900

    Fix itemToDo - layout

--- src/components/features/todo/itemTodo/ItemTodo.jsx
+++ src/components/features/todo/itemTodo/ItemTodo.jsx
@@ -1,3 +1,5 @@
+import format from 'date-fns/format';
+
 import Button from '@/components/ui/button/Button';
 
 import { useApi } from '@/hooks/useApi';
@@ -13,37 +15,36 @@ const ItemTodo = ({ item, setTodoItemEdit, setIsOpenEdit }) => {
   const { removeTodo } = useApi();
 
   return (
-    <>
-      <li className={styles.ToDoItem}>
-        <div className={styles.todoDetails}>
-          <CheckBox todoId={item._id} checked={item.complete} />
-          <div className={styles.todoTexts}>
-            <span>{item.name}</span>
-            <span>{item.createdAt}</span>
-          </div>
-        </div>
+    <li className={styles.ToDoItem}>
+      <div className={styles.todoActions}>
+        <CheckBox todoId={item._id} checked={item.complete} />
+      </div>
 
-        <div className={styles.todoActions}>
-          <Button
-            className={styles.buttonSvg}
-            onClick={() => {
-              removeTodo.mutate(item._id);
-            }}
-          >
-            <TrashImage />
-          </Button>
-          <Button
-            className={styles.buttonSvg}
-            onClick={() => {
-              setTodoItemEdit(item);
-              setIsOpenEdit(true);
-            }}
-          >
-            <EditImage />
-          </Button>
-        </div>
-      </li>
-    </>
+      <div className={styles.todoDetails}>
+        <p className={styles.text}>{item.name}</p>
+        <p>{format(new Date(item.createdAt), 'dd MMM yyyy')}</p>
+      </div>
+
+      <div className={styles.todoActions}>
+        <Button
+          className={styles.buttonSvg}
+          onClick={() => {
+            removeTodo.mutate(item._id);
+          }}
+        >
+          <TrashImage />
+        </Button>
+        <Button
+          className={styles.buttonSvg}
+          onClick={() => {
+            setTodoItemEdit(item);
+            setIsOpenEdit(true);
+          }}
+        >
+          <EditImage />
+        </Button>
+      </div>
+    </li>
   );
 };
 
